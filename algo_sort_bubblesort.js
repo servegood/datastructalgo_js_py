4a0ebf4f356a7a5fd58086c9e34aa64a9b5cd4a3
Waheeda Dudekula <dwaheeda@gmail.com>
2022-07-26 09:51:21 -0500

    bubblesort

---
+++ algo_sort_bubblesort.js
@@ -0,0 +1,20 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+//Time Complexity O (n^2) for average & worst - because it loops thru every element and keeps swapping
+//till done. Space Complexity if O(1)
+function bubbleSort(array) {
+  const length = array.length
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        //Swap the numbers
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+    }
+  }
+}
+
+bubbleSort(numbers)
+console.log(numbers)
